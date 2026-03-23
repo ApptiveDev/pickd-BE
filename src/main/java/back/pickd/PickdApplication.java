367f2857def27c70c129f14d11621e409e161b52
tomchccom <dreamkms2014@gmail.com>
2026-03-23 22:10:35 +0900

    initail commit

---
+++ src/main/java/back/pickd/PickdApplication.java
@@ -0,0 +1,13 @@
+package back.pickd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickdApplication.class, args);
+	}
+
+}
