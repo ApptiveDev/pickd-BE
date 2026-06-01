e9064f065e8b65a9e636ed321254a3775c3cd496
Hyun <tmdals0429@pusan.ac.kr>
2026-06-01 13:24:53 +0900

    Back/feat/cr (#20)
    * feat: 경험 수기 생성 DTO 및 Repository 추가
    
    * feat: UserExperience 경험 CR Service/Controller 구현
    
    * feat: 필터링 조회 추가

---
+++ src/main/java/back/pickd/user/dto/ExperienceCreateResponseDto.java
@@ -0,0 +1,11 @@
+package back.pickd.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// 경험 생성 응답 DTO
+@Getter
+@AllArgsConstructor
+public class ExperienceCreateResponseDto {
+    private final String id;
+}
