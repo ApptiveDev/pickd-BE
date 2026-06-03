76391c9617c3474fcdbfa3d91721c2b54fce96c2
myeongsung kim <dreamkms2014@pusan.ac.kr>
2026-06-03 21:24:54 +0900

    Update DocumentRequest.java

--- src/main/java/back/pickd/document/dto/DocumentRequest.java
+++ src/main/java/back/pickd/document/dto/DocumentRequest.java
@@ -9,7 +9,7 @@ public class DocumentRequest {
     private String title;
     private String company;
     private String type; // 포트 폴리오인지 이력서인지
-    private Integer progress;
-    private String status;
-    private String content;
+    private Integer progress; // 글자수로 계산해서 응답 
+    private String status; // nullable 
+    private String content; // 수정 가능해야함. 
 }
