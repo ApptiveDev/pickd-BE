4714ec6574d3c87400e7607bf9866fa7cb2e7bb1
myeongsung kim <dreamkms2014@pusan.ac.kr>
2026-06-03 21:23:48 +0900

    Update DocumentRequest.java

--- src/main/java/back/pickd/document/dto/DocumentRequest.java
+++ src/main/java/back/pickd/document/dto/DocumentRequest.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 public class DocumentRequest {
     private String title;
     private String company;
-    private String type;
+    private String type; // 포트 폴리오인지 이력서인지
     private Integer progress;
     private String status;
     private String content;
